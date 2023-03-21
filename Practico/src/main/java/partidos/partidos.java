305b8536672bc6e0270580afb30cf443cf00becb
FranDesarrolla <franciscovargascontacto@gmail.com>
2023-03-21 12:16:46 -0300

    Versión 1.0
    Agregue partidos.java
    Agregue resultados.java
    No funciona todavia

---
+++ Practico/src/main/java/partidos/partidos.java
@@ -0,0 +1,28 @@
+package partidos;
+package resultados;
+
+public class partidos { 
+    public static void main(String[] args) {
+    
+        // Primer partido Argentina vs Polonia
+        // Segundo partido Mexico vs Arabia Saudita
+        // Tercer partido Holanda vs Ecuador
+
+        String persona1 = "Pedro";
+        
+        // Resultados propuestos por Pedro para Argentina vs Polonia
+        
+        int Resultados1 = 1; //Gana Argentina
+        
+        // Resultados teniendo en cuenta los resultados oficiales
+
+        if (1 == Resultados1) {
+            System.out.println(persona1 + " acerto, obtiene 1 punto");
+        } else if (2 == Resultados1){
+            System.out.println(persona1 + " no acerto, obtiene 0 puntos");
+        } else {
+            System.out.println(persona1 + " no acerto, obtiene 0 puntos");
+        }
+    }
+    }
+}
---
+++ Practico/src/main/java/partidos/resultados.java
@@ -0,0 +1,28 @@
+package partidos;
+
+public class resultados {   
+
+    public static void main(String[] args) { 
+   
+    // 1 = Equipo 1 Ganador
+    // 2 = Empate
+    // 3 = Equipo 2 Ganador
+      
+    // Goles del Partido Oficial
+
+    int ArgGol = 2;
+    int PolGol = 1;
+        
+    int AraGol = 1;
+    int MexGol = 2;
+        
+    int HolGol = 1;
+    int EcuGol = 1;
+    
+    // Resultados de los partidos oficiales
+    
+    int ArgPol = 1;
+    int HolEcu = 2;
+    int AraMex = 3;
+    
+}
